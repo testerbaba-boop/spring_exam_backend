c4baff12bf8bdcdbf27c415064551a550f0379d0
testerbaba-boop <hw3981192@gmail.com>
2024-09-13 05:54:01 +0800

    增加单测

---
+++ src/test/java/lsgwr/exam/service/ExamServiceTest.java
@@ -0,0 +1,12 @@
+package lsgwr.exam.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamServiceTest {
+
+    @Test
+    void create() {
+    }
+}
---
+++ src/test/java/lsgwr/exam/service/UserServiceTest.java
@@ -0,0 +1,14 @@
+package lsgwr.exam.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void register() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void login() {
+    }
+}
